7668228de4c540857c04d1b97de4826e4cda1fe1
Jón Levy <nonni@nonni.cc>
2017-04-19 13:36:27 +0000

    square aggregation and word category

--- refugee_app/refugee_app/refugees/views.py
+++ refugee_app/refugee_app/refugees/views.py
@@ -1,9 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
-from refugees.models import Post, FacebookPost, Comment
+from refugees.models import Post, FacebookPost, Comment, WordCategory
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import Http404
+from django.db.models import Count
+
 import json
 from nltk import FreqDist, bigrams
 from collections import Counter
@@ -12,9 +14,30 @@ import requests
 def bubbles(request):
     return render(request, 'refugees/bubbles.html')
 
+def aggregation(request):
+    #q = WordCategory.objects.annotate(Count('comment'))[:100]
+    posts = Comment.objects.values_list('body', flat=True)
+    wc = WordCategory.objects.values_list('word', flat=True)
+    
+    word_list = []
+    wc_list = []
+    result = []
+    for post in posts:
+        if post:
+            for item in post.split():
+                word_list.append(item)
+
+    for word in wc:        
+        wc_list.append(word)
+
+    result = [{'word': word, 'count': word_list.count(word[:4])} for word in wc_list]
+    #q = WordCategory.objects.annotate(Count('comment'))[:100]
+    return render(request, 'refugees/aggregation.html', {'items': sorted(result, key=lambda item: item['count'], reverse=True)})
+
 @csrf_exempt
 def api_post(request):
     posts = Comment.objects.values_list('body', flat=True)
+    
 
     word_list = []
     result = []
